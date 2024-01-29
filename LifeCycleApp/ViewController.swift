62eec7ec4e4b3b5c776d4503df5a826b0558e9e2
Maxim Makarov <makarovmaxqa@gmail.com>
2024-01-29 22:36:01 +0300

    Initial Commit

---
+++ LifeCycleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeCycleApp
+//
+//  Created by Maxim Makarov on 29.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
