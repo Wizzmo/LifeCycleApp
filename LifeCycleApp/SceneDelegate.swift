e09d957117fd93d849263cacca806d1bd2b31c77
Maxim Makarov <makarovmaxqa@gmail.com>
2024-02-12 23:13:34 +0300

    Analyzed the logic of the life cycle of the app

--- LifeCycleApp/SceneDelegate.swift
+++ LifeCycleApp/SceneDelegate.swift
@@ -11,26 +11,40 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
+    // Вызываеся, когда новая сцена добавляется в приложение
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
+        UIView.appearance().tintColor = .white
+        
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.backgroundColor = .systemGray
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+        print(#function)
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
+    
+    // Вызывается перед переходом сцены в активное состояние
     func sceneWillEnterForeground(_ scene: UIScene) {
+        print(#function)
+    }
+    
+    // Вызывается, когда сцена становится активной
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        print(#function)
+    }
+    
+    // Вызывается перед переходом приложения в фоновый режим
+    func sceneWillResignActive(_ scene: UIScene) {
+        print(#function)
+    }
+    
+    // Вызывается после перехода приложения в фоновый режим
+    func  sceneDidEnterBackground(_ scene: UIScene) {
+        print(#function)
     }
 
-    func sceneDidEnterBackground(_ scene: UIScene) {
-    }
-
+//    func sceneDidDisconnect(_ scene: UIScene) {
+//    }
 
 }
 
